ceda0d2e5906d84ed49b00f99bd1b35622ee109a
Eric Junard Dabon <eric.junard.dabon@gmail.com>
2015-12-05 21:14:11 +0800

    initial commit

---
+++ header.php
@@ -0,0 +1,37 @@
+<head>
+  <link rel="stylesheet/less" href="less/style.less">
+  <script src= "js/less.js" type="text/JavaScript"> </script>
+  <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+  <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default  navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">pretty<span class="blue">good</span>look</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">SUBMIT</a></li>
+        <li><a href="#">FEEDBACK</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+    <title>Prett Good Look</title>
+    <?php include ("header.php");?>
+  </head>
+  <body>
+    <!--Start of first fold-->
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="ff-top">SHOP VIDEOS, OR SUBMIT VIDEOS FOR SHOPPERS!</h1>
+            <h4>Lorem ipsum dolor sit amet, soleat latine ornatus at est</h4>
+            <div class="row">
+              <div class="col-md-6"><a href="shop-videos" class="btn btn-primary">SHOP VIDEOS</a></div>
+              <div class="col-md-6"></div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <img src="dist/img/girl.png" class="ff-img-padding" >
+          </div>
+        </div>
+      </div> <!-- /container -->
+    <!--End of first fold-->
+  
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="dist/js/bootstrap.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
