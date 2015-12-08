ee7ef3a00d78a0716fd84ccf80ace9ae42d1b74c
Eric Junard Dabon <eric.junard.dabon@gmail.com>
2015-12-09 00:33:10 +0800

    coded about page

---
+++ about.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+    <title>Pretty Good Look</title>
+    <?php include ("header.php");?>
+  </head>
+  <body>
+  <section class="about">
+    <div class="container">
+      <div class="col-md-6">
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. Eu errem tempor phaedrum sed, soleat sanctus eos no, pri ad hinc maiorum consectetuer.</p>
+      </div>
+    </div>
+  </section>
+  <section class="about-info">
+    <div class="container">
+      <div class="col-md-6">
+      <h2>More about PGL</h2>
+      <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. Eu errem tempor phaedrum sed, soleat sanctus eos no, pri ad hinc maiorum consectetuer.</p>
+      <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. Eu errem tempor phaedrum sed, soleat sanctus eos no, pri ad hinc maiorum consectetuer.</p>
+      <input type="submit" class="btn btn-default half-btn" value="LEARN MORE">
+     </div>
+    </div>
+  </section>
+  <section class="for-sy">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h2>FOR YOUTUBERS</h2>
+          <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. Eu errem tempor phaedrum sed, soleat sanctus eos no, pri ad hinc maiorum consectetuer. Zril discere qualisque ad his. Mea errem viderer elaboraret et, error feugait ad eos, eirmod tamquam numquam ius ei. Solet placerat id pro.</p>
+          <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. </p>
+          <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. Eu errem tempor phaedrum sed, soleat sanctus eos no, pri ad hinc maiorum consectetuer. Zril discere qualisque ad his. Mea errem viderer elaboraret et, error feugait ad eos, eirmod tamquam numquam ius ei. Solet placerat id pro.</p>
+          <input type="submit" class="btn btn-primary half-btn" value="Submit Videos">
+        </div>
+          <div class="col-md-6">
+          <h2>FOR SHOPPERS</h2>
+          <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. Eu errem tempor phaedrum sed, soleat sanctus eos no, pri ad hinc maiorum consectetuer. Zril discere qualisque ad his. Mea errem viderer elaboraret et, error feugait ad eos, eirmod tamquam numquam ius ei. Solet placerat id pro.</p>
+          <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. </p>
+          <p>Lorem ipsum dolor sit amet, intellegat referrentur mea ei, cu ornatus deterruisset quo. Nostrum patrioque no mea. Eu errem tempor phaedrum sed, soleat sanctus eos no, pri ad hinc maiorum consectetuer. Zril discere qualisque ad his. Mea errem viderer elaboraret et, error feugait ad eos, eirmod tamquam numquam ius ei. Solet placerat id pro.</p>
+          <input type="submit" class="btn btn-primary half-btn" value="Shop Videos">
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php include ("footer.php");?>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="dist/js/bootstrap.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
