04c7ca194df9b6cf3d2fd8ba87a29e9f2531ee81
Eric Junard Dabon <eric.junard.dabon@gmail.com>
2015-12-09 23:13:57 +0800

    coded faq page

---
+++ faq.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+    <title>Pretty Good Look</title>
+    <?php include ("header.php");?>
+  </head>
+  <body>
+  <section class="contact">
+    <div class="container">
+      <div class="col-md-8 col-md-offset-2">
+          <h2 class="text-center ">FREQUENTLY ASKED QUESTIONS</h2>
+         <div class="faq">
+            <p class="title">Question  ipsum dolor sit  ?</p>
+            <p>Answer  ipsum dolor sit amet soleat latine sit  amet soleat latine sit.ipsum dolor sit amet soleat latine sit  amet 
+               soleat latine sit.
+            </p><!--end of faq 1-->
+            <p class="title">Question  ipsum dolor sit  ?</p>
+            <p>Answer  ipsum dolor sit amet soleat latine sit  amet soleat latine sit.ipsum dolor sit amet soleat latine sit  amet 
+               soleat latine sit.
+            </p><!--end of faq 2-->
+            <p class="title">Question  ipsum dolor sit  ?</p>
+            <p>Answer  ipsum dolor sit amet soleat latine sit  amet soleat latine sit.ipsum dolor sit amet soleat latine sit  amet 
+               soleat latine sit.
+            </p><!--end of faq 3-->
+            <p class="title">Question  ipsum dolor sit  ?</p>
+            <p>Answer  ipsum dolor sit amet soleat latine sit  amet soleat latine sit.ipsum dolor sit amet soleat latine sit  amet 
+               soleat latine sit.
+            </p><!--end of faq 4-->
+            <p class="title">Question  ipsum dolor sit  ?</p>
+            <p>Answer  ipsum dolor sit amet soleat latine sit  amet soleat latine sit.ipsum dolor sit amet soleat latine sit  amet 
+               soleat latine sit.
+            </p><!--end of faq 5-->
+            <p class="title">Question  ipsum dolor sit  ?</p>
+            <p>Answer  ipsum dolor sit amet soleat latine sit  amet soleat latine sit.ipsum dolor sit amet soleat latine sit  amet 
+               soleat latine sit.
+            </p><!--end of faq 6-->
+              <p class="title">Question  ipsum dolor sit  ?</p>
+            <p>Answer  ipsum dolor sit amet soleat latine sit  amet soleat latine sit.ipsum dolor sit amet soleat latine sit  amet 
+               soleat latine sit.
+            </p><!--end of faq 7-->
+         </div>
+       
+      </div>
+    </div>
+  </section>
+    <?php include ("footer.php");?>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="dist/js/bootstrap.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
